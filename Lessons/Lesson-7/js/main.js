212c58b7df495fd0d3d563acda447ebccc0f5e06
Horbatiuk Liubomyr <horbatiuk.liubomyr@mtc.co.uk>
2024-02-21 02:46:17 +0200

    Update menu for all pages

--- Lessons/Lesson-7/js/main.js
+++ Lessons/Lesson-7/js/main.js
@@ -1,1 +1,11 @@
-console.log('test');
+document.addEventListener('DOMContentLoaded', function () {
+    const headerContainer = document.getElementById('header-container');
+
+    if (!headerContainer) return;
+
+    fetch('../../page-parts/header.html')
+        .then(response => response.text())
+        .then(data => {
+            headerContainer.innerHTML = data;
+        });
+});
